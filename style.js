1d6febdb10fd527f59f14fb7e4cbe681e93db1d0
nickmac <nickpmacintyre@gmail.com>
2016-02-05 13:59:08 -0700

    random box generator

--- style.js
+++ style.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-  $('main').hover(function() {
-        $('div').append("<li>" + "</li>");
+  $('main').click(function() {
+    var numRand = Math.floor(Math.random() * 700);
+    var numRand1 = Math.floor(Math.random() * 700);
+    function makeid(){
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( var i=0; i < 5; i++ )
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+  }
+        var temp = makeid();
+        $('body').append("<li class='" + temp + "'>" + numRand + ', ' + numRand1 + "</li>");
+        $('.' + temp).css('top', numRand);
+        $('.' + temp).css('left', numRand1);
+
     });
   });
